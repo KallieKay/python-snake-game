dac5ef789539e08a10669bd751949e222482ea96
wtc <kasmiswgc025@student.wethinkcode.co.za>
2026-04-09 08:35:48 +0200

    adding food at random positions and detection of collision to a wall or to the snake itself

--- snakey.py
+++ snakey.py
@@ -14,10 +14,36 @@ while running:
 
 pygame.quit()
 
+
+"""think of the snake as a list of blocks"""
 snake = [(100, 100), (90, 100), (80, 100)]
 snake_size=10
 
+"""drawing the snake"""
 for segment in snake:
     pygame.draw.rect(screen, (0, 255, 0), (segment[0], segment[1], snake_size, snake_size ))
 
-    
+"""using direction variables for movement"""
+dx=10
+dy=0
+
+"""update snake position each frame"""
+head=snake[0]
+new_head=(head[0]+dx, head[1]+dy)
+snake.insert(0, new_head)
+snake.pop()
+
+"""keyboard controls"""
+keys=pygame.key.get_pressed()
+
+if keys[pygame.K_UP]:
+    dx, dy =0, -10
+elif keys[pygame.K_DOWN]:
+    dx, dy= 0, 10
+elif keys[pygame.K_LEFT]:
+    dx, dy= -10, 0
+elif keys[pygame.K_RIGHT]:
+    dx, dy=10, 0
+
+"""adding food"""
+
